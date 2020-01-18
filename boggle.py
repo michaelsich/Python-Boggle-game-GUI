88b527812711579ab614deb1219c3a58206259e0
Micahel Sichenko <micha741@gmail.com>
2020-01-18 23:57:32 +0200

    project start

---
+++ boggle.py
@@ -0,0 +1,7 @@
+# # # # # # #  EX12  # # # # # # #
+# ID        : 316138007         &&
+# NAME      : Michael Sichenko  && Or Katz
+# LOGIN     : m_sich            && orkatz
+# COMMENTS  :
+# # # # # # # # # # # # # # # #
+import tkinter
