7837b57cd84ac7cd7a6b4299c1541a0faaf04d54
michaelsich <58555518+michaelsich@users.noreply.github.com>
2020-01-23 00:38:56 +0200

    Update boggle.py

--- boggle.py
+++ boggle.py
@@ -5,3 +5,21 @@
 # COMMENTS  :
 # # # # # # # # # # # # # # # #
 import tkinter
+import Game
+import Cube
+
+def game_loop(game):
+    # TODO - create cubes
+    matrix = game.get_matrix()
+    game_cubes = []
+    for row in matrix:
+        for letter in row:
+            cube = Cube(letter)
+        game_cubes.append(cube)
+
+
+game = Game()
+
+
+if __name__ == '__main__':
+    pass
