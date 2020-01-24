5f83a91c26ee657be15085cacd4f8c33bc70877a
Micahel Sichenko <micha741@gmail.com>
2020-01-24 22:15:59 +0200

    gui looks good

--- GUI.py
+++ GUI.py
@@ -2,14 +2,14 @@ from tkinter import *
 import Cube
 
 # window properties
-WIDTH       = 800      # window width in pixels
+WIDTH       = 700      # window width in pixels
 HEIGHT      = 500      # window height in pixels
 
 # font settings
 H1_FONT     = ("arial", 16, "bold")
 BUTTON_FONT = ("arial", 14)
-MAIN_FONT   = ("arial", 12)
-CUBE_FONT   = ("david", 12)
+MAIN_FONT   = ("arial", 12, "bold")
+CUBE_FONT   = ("david", 36, "bold")
 TXT_BOX_FONT= ("david", 13)
 
 # images
@@ -27,6 +27,16 @@ class Graphics:
         self.__root = self.init_main_window()
         self.frames = {}
 
+        # game elements
+        self.__game_cubes = []
+        self.__elements = {
+            "canvas"    : "",
+            "textbox"   : "",
+            "time"      : "",
+            "score"     : ""
+        }
+
+        # create frames
         self.create_main_frame()
         self.create_game_frame()
 
@@ -37,7 +47,7 @@ class Graphics:
         gui_main = Tk()
         gui_main.geometry(f"{WIDTH}x{HEIGHT}")
         gui_main.resizable(width=False, height=False)
-        gui_main.title("HUJI Boggle")
+        gui_main.title("HUJI Boggle!")
         return gui_main
 
     def create_main_frame(self):
@@ -93,52 +103,70 @@ class Graphics:
 
         # time
         time_frame = Frame(right_frame, **debug_style)
-        # TODO: add time support
-        time_label = Label(time_frame, text="time here")
+        time_label = Label(time_frame, text="time here", font=MAIN_FONT)
+        self.__elements["time"] = time_label
 
         # score
         score_frame = Frame(right_frame, **debug_style)
-        score_label = Label(score_frame, text="Score:")
+        score_label = Label(score_frame, text="Score:", font=MAIN_FONT)
+        self.__elements["score"] = score_label
 
         # textbox
         words_frame = Frame(right_frame, **debug_style)
-        words_box   = Text(words_frame, state="disabled",
-                           cursor="pirate", relief="groove", bg='light grey',
-                           width=20, font=TXT_BOX_FONT)
+        words_box   = Text(words_frame, state="disabled", cursor="pirate",
+                           relief="groove", bg='light grey', spacing1=10,
+                           width=20, height=14, padx=10, font=TXT_BOX_FONT)
+        self.__elements["textbox"] = words_box
+
+        # quit button
+        quit_button = Button(right_frame, text="Quit",
+                             bg="dark red", fg="white", relief="solid",
+                             font=BUTTON_FONT,
+                             command=self.on_quit_click_event)
 
         # packing objects
         game_frame.grid(row=0, column=0, rowspan=3, sticky='news')
         left_frame.grid(row=0, column=1, padx=10, pady=10, sticky='nse')
         right_frame.grid(row=0, column=2, padx=10, pady=10, sticky='nsw')
-        score_frame.pack()
-        score_label.pack()
-        time_frame.pack()
+        time_frame.pack(fill=X)
         time_label.pack()
+        score_frame.pack(fill=X)
+        score_label.pack(side=LEFT)
         words_frame.pack(fill=Y)
         words_box.pack()
+        quit_button.pack(fill=X)
 
     def __create_game_board(self, frame):
-
+        """ Creates a canvas with cubes in the given frame """
         # create image objects for canvas
         bg_img = self.__letters[0][0].get_background()
         img = PhotoImage(file=bg_img)
 
         # create a canvas
-        canvas = Canvas(frame,height=450, width=500, bg="white")
+        canvas = Canvas(frame, height=450, width=450, bg="dark blue")
+        canvas.config(scrollregion=canvas.bbox(ALL))
 
-        # TODO: find out why cant see background image
+        # fill cubes ( pic + letter )
         for row in range(len(self.__letters)):
             curr_letters = self.__letters[row]
             for col in range(len(curr_letters)):
                 pos_x = row*110+60
                 pos_y = col*110+60
-                image = canvas.create_image(pos_x, pos_y, image=img)
+
+                # create cube
+                image = canvas.create_image(pos_y, pos_x, image=img, command=self.on_start_click_event())
                 canvas.background = img
-                canvas.create_text(pos_x, pos_y, fill="darkblue", font="Times 20 italic bold",
-                                        text=curr_letters[col].get_letter())
+                canvas.create_text(pos_y, pos_x, fill="darkblue", font=CUBE_FONT,
+                                   text=curr_letters[col].get_letter())
+                self.__game_cubes.append(image)
+
+                # create event handling
+                # TODO: configure how to change background
+                canvas.tag_bind(image, '<ButtonPress-1>',
+                                lambda event, arg=image: self.on_cube_click_event(event, arg))
+
+        self.__elements["canvas"] = canvas
         canvas.pack()
-
-
 
     # region EVENTS
     def on_start_click_event(self):
@@ -147,8 +175,32 @@ class Graphics:
 
     def on_quit_click_event(self):
         """Event handler for quit button click"""
-        pass
+        # TODO: add message box first
+        self.__root.destroy()
+
+    def on_cube_click_event(self, event, index):
+        self.add_word_to_box("abc\n")
+        print(index)
+
     # endregion EVENTS
+
+    # region SETTERS
+    def add_word_to_box(self, word):
+        """ Gets a word and adds it to the text box """
+        self.__elements["textbox"].config(state=NORMAL)
+        self.__elements["textbox"].insert(0.0, word)
+        self.__elements["textbox"].config(state=DISABLED)
+
+    def set_time(self, new_time):
+        """ updates the game time """
+        self.__elements["time"].config(text=new_time)
+
+    def set_score(self, new_score):
+        """ updates the game time """
+        self.__elements["score"].config(text=new_score)
+
+    # endregion SETTERS
+
 
 
 if __name__ == "__main__":
@@ -157,7 +209,7 @@ if __name__ == "__main__":
         cubbe = Cube.Cube(let)
         lst[0].append(cubbe)
 
-    for let in ['e','f','g','k']:
+    for let in ['e','f','Qe','k']:
         cubbe = Cube.Cube(let)
         lst[1].append(cubbe)
 
